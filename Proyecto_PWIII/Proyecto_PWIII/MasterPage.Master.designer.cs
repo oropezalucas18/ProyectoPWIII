32ea0585c4ed4456876a69abcc29c3c07ea9c7cc
oropezalucas <73489024+oropezalucas@users.noreply.github.com>
2021-10-08 22:00:37 -0400

    Añadiendo Encabezado

--- Proyecto_PWIII/Proyecto_PWIII/MasterPage.Master.designer.cs
+++ Proyecto_PWIII/Proyecto_PWIII/MasterPage.Master.designer.cs
@@ -33,12 +33,12 @@ namespace Proyecto_PWIII
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// ContentPlaceHolderBody control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolderBody;
     }
 }
