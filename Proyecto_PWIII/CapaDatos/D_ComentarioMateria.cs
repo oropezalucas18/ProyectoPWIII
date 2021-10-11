2456ab62c4deed415931a78087af5d1f7bb3e029
AleUlloa2504 <65233048+AleUlloa2504@users.noreply.github.com>
2021-10-10 20:00:04 -0400

    Consultas sql

--- Proyecto_PWIII/CapaDatos/D_ComentarioMateria.cs
+++ Proyecto_PWIII/CapaDatos/D_ComentarioMateria.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
-using Entidades;
 using Microsoft.Practices.EnterpriseLibrary.Data;
 
 namespace CapaDatos
